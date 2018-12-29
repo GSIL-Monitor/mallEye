e30ea41ebe2383b24db0116895b88bf8aae7a22d
YUANZHIFAN <843337681@qq.com>
2018-12-29 18:10:38 +0800

    task

--- mall.py
+++ mall.py
@@ -24,29 +24,42 @@ class winshang(unittest.TestCase):
     def test_login(self):
         # 登录
         main = method(self.driver)
-        main.input("xpath", "//*[@id='login']/div[2]/div[2]/div[2]/div/input", "nini317")
-        main.input("xpath", "//*[@id='login']/div[2]/div[2]/div[3]/div/input", "12345678")
+        main.input("xpath", "//*[@id='login']/div[2]/div[2]/div[2]/div/input", "wsmall")
+        main.input("xpath", "//*[@id='login']/div[2]/div[2]/div[3]/div/input", "ws2018")
         main.click_element("xpath", "//*[@id='login']/div[2]/div[2]/div[4]")
+
+        name = main.get_text('xpath', '//*[@id="app"]/div[1]/div[3]/span[2]')
+        try:
+            self.assertEqual('wsmall_679732130', name)
+        except AssertionError as e:
+            print(e)
+        
         self.driver.implicitly_wait(10)
 
-        main.assertEql('xpath', '//*[@id="app"]/div[1]/div[3]/span[2]', 'Hi!   test011')
+        # 获取两天前的日期
+        now_time = datetime.datetime.now()
+        change_time = now_time + datetime.timedelta(days=-2)
+        change_time_format = change_time.strftime('%Y-%m-%d')
+        
+        now_data = main.get_text('xpath', '//*[@id="app"]/div[2]/div[1]/div[2]/div/div[2]/div[1]/span[2]')
+        now_data_format = now_data[0:10]
+
+        try:
+            self.assertEqual(now_data_format, change_time_format)
+        except AssertionError as e:
+            print(e)
 
     def test_report_day(self):
         # 日/周/月报
         # 获取当前时间
         # date = time.strftime("%Y-%m-%d", time.localtime())
 
-        # 两天前的日期
-        '''
-        now_time = datetime.datetime.now()
-        change_time = now_time + datetime.timedelta(days=-2)
-        change_time_format = change_time.strftime('%Y-%m-%d')
-        print(change_time_format)
-        '''
+        
         main = method(self.driver)
-        main.click_element('xpath', '//*[@id="app"]/div[1]/div[2]/ul/li[2]/span')
+        main.click_element('xpath', '//*[@id="app"]/div[2]/div[1]/div[1]/div/div[2]/div/span')
+        
         self.driver.implicitly_wait(10)
-        time.sleep(3)
+        
 
 
 
--- mall_method.py
+++ mall_method.py
@@ -1,23 +1,5 @@
 #!/usr/bin/env python
 # -- coding: utf-8 --
-import unittest
-
-u'''
-    1	assertEqual(a, b)	判断ab是否相等
-    2	assertNotEqual(a, b)	判断ab不相等
-    3	assertIs(a, b)　	判断a是b
-    4	assertIsNot(a, b)	判断a不是b
-    5	assertIsNone(a)	判断a是不是None
-    6	assertIsNotNone(a)	判断a不是None
-    7	assertIn(a,b)	判断a在b中，此时a与b可以相等
-    8	assertNotIn(a, b)	判断a不在b中
-    9	assertIsInstance(a, b)	判断a是否属于b的实例
-    10	assertNotIsInstance(a, b)	判断a不属于b的实例 
-    11	assertGreater(a, b)	判断a > b
-    12	assertGreaterEqual(a, b)	判断a >= b 
-    13	assertLess(a, b)	判断a < b 
-    14	assertLessEqual(a, b)	判断a <= b 
-'''
 
 class method ():
     def __init__(self, driver):
@@ -35,9 +17,3 @@ class method ():
     # 获取元素文本内容
     def get_text(self, type, value):
         return self.dr.find_element(type, value).text
-
-    # 判断内容是否存在相等
-    def assertEql(self, type, loction, value):
-        content = self.dr.find_element(type, loction).text
-        print(content)
-        self.assertEqual(value, content)
